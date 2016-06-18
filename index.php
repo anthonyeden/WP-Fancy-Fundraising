9ea27f3351c32aec8dd3f6846afc611a399892d6
Anthony Eden <anthony@mediarealm.com.au>
2016-06-18 20:59:19 +1000

    Add GPL2 license to header of index.php and attach LICENSE.md file with the full license text.

--- index.php
+++ index.php
@@ -6,6 +6,22 @@ Description: Layout tweaks and widgets for Fundraising with Wordpress
 Version: 1.0.5
 Author: Media Realm
 Author URI: http://www.mediarealm.com.au/
+
+License: GPL2
+
+WP Fancy Fundraising is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+WP Fancy Fundraising is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with WP Fancy Fundraising. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+
 */
 
 class WPFancyFundraising {
