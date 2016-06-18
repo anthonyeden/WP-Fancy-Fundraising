6a071e5a214656fac89372676e8e6a2f3a5b68e8
Anthony Eden <anthony@mediarealm.com.au>
2016-06-18 20:55:11 +1000

    Add class "radio-button-buttons" to a radio button field to turn the radio buttons into big, pressable buttons.

--- template-donate1.php
+++ template-donate1.php
@@ -105,6 +105,12 @@ add_filter('gform_field_content',     array('WPFancyFundraising', 'gf_column'), 
         });
         <?php } ?>
 
+        jQuery('.radio-button-buttons li label').first().addClass("active");
+        jQuery('.radio-button-buttons li label').on('click touchstart', function(){
+            jQuery('.radio-button-buttons li label').removeClass("active");
+            jQuery(this).addClass("active");
+        });
+
     </script>
 
     <?php
